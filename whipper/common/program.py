2ab5d9c80cf1c574c6f4c9c7db13e6fd9df63ff6
Michael Booth <mbooth@hullstreetenergy.com>
2019-01-15 14:10:26 -0500

    'year' is not always present in cddb data.

--- whipper/common/program.py
+++ whipper/common/program.py
@@ -293,7 +293,10 @@ class Program:
                 if key == 'DTITLE':
                     dtitle += value
                 elif key == 'DYEAR':
-                    data['year'] = int(value)
+                    try:
+                        data['year'] = int(value)
+                    except:
+                        pass # or should we put in 0?
                 elif key == 'DGENRE':
                     data['genre'] = value
                 elif key.startswith('TTITLE'):
